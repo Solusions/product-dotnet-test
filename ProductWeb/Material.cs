514cf9bbbd39d917050d151ea1cd89309d6777f9
gurvindervirk <44731478+gurvindervirk@users.noreply.github.com>
2019-10-24 16:23:15 -0400

    Commit Changes
    Added a new Controller with the name of Materials and also created Edit, Create  View for Materials Controller. To Add Dropdown list in edit view do changes in Material.cs in product.data project and add new properties for Material list. Also did changes in Layout in action link for materials index.

---
+++ ProductWeb/Material.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Product.Data.Entities
+{
+    public class Material
+    {
+        [Key]
+        public Int32 MaterialId { get; set; }
+
+        public String Name { get; set; }
+
+        public Decimal Cost { get; set; }
+
+        public virtual IEnumerable<Material> ExecutivesList { get; set; }
+
+
+        [InverseProperty("Material")]
+        public ICollection<ProductMaterial> ProductMaterials { get; set; }
+    }
+}
---
+++ ProductWeb/MaterialsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Product.Data.Context;
+using Product.Data.Entities;
+
+namespace ProductWeb.Controllers
+{
+    public class MaterialsController : Controller
+    {
+        private ProductDataContext db = new ProductDataContext();
+
+        // GET: Materials
+        public ActionResult Index()
+        {
+            return View(db.Materials.ToList());
+        }
+
+        // GET: Materials/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Material material = db.Materials.Find(id);
+            if (material == null)
+            {
+                return HttpNotFound();
+            }
+            return View(material);
+        }
+
+        // GET: Materials/Create
+        public ActionResult Create()
+        {
+            Material model = new Material();
+            model.ExecutivesList = db.Materials;
+            return View(model);
+        }
+
+        // POST: Materials/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaterialId,Name,Cost")] Material material)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Materials.Add(material);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(material);
+        }
+
+        // GET: Materials/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Material material = db.Materials.Find(id);
+            material.ExecutivesList = db.Materials;
+            if (material == null)
+            {
+                return HttpNotFound();
+            }
+            return View(material);
+          
+        }
+     
+
+       // POST: Materials/Edit/5
+       // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+       // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+       [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit( FormCollection obj)
+        {
+        
+                using (var db1 = new ProductDataContext())
+                {
+                //var myData = db1.ProductMaterials.Where(x => x.MaterialId == material.MaterialId).ToList();
+                //myData.ForEach(m => m.MaterialId = int.Parse(obj["hidden1"]));
+                //db.SaveChanges();
+                Int32 deleteId = int.Parse(obj["hidden1"]);
+                Int32 keepId = int.Parse(obj["hidden2"]);
+
+                    var query = db1.ProductMaterials.Where(p => p.MaterialId == deleteId).ToList();
+                    foreach(var post in query)
+                    {
+                        post.MaterialId =keepId;
+                    }
+                    db1.SaveChanges();
+
+                var itemToRemove = db.Materials.SingleOrDefault(x => x.MaterialId == deleteId);
+               
+                if (itemToRemove != null)
+                {
+                    db.Materials.Remove(itemToRemove);
+                    db.SaveChanges();
+                   
+                }
+                return RedirectToAction("Index");
+                  
+                //material.Name = obj["hidden1"].ToString();
+                //db.Entry(material).State = EntityState.Modified;
+                //db.SaveChanges();
+                //return RedirectToAction("Index");
+            }
+           
+        }
+
+        //public ActionResult Edit(FormCollection obj)
+        //{
+
+        //    using (var db1 = new ProductDataContext())
+        //    {
+        //        var myData = db1.ProductMaterials.Where(x => x.MaterialId == int.Parse(obj["hidden1"])).ToList();
+        //        myData.ForEach(m => m.MaterialId = int.Parse(obj["hidden1"]));
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        // }
+        // GET: Materials/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Material material = db.Materials.Find(id);
+            if (material == null)
+            {
+                return HttpNotFound();
+            }
+            return View(material);
+        }
+
+        // POST: Materials/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Material material = db.Materials.Find(id);
+            db.Materials.Remove(material);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
